acc974791ed47ff4df1882b6ff76491e857065fb
Ajay Kumar Reddy <reddyajay155@gmail.com>
2026-07-04 19:13:00 +0530

    Improve Smart Expense Tracker and remove tracked cache files

--- expense-frontend/src/pages/Login.js
+++ expense-frontend/src/pages/Login.js
@@ -1,14 +1,11 @@
 import { useState } from "react";
 
 function Login() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-
     try {
-
       const formData = new URLSearchParams();
 
       formData.append("username", email);
@@ -17,50 +14,37 @@ function Login() {
       const res = await fetch("http://127.0.0.1:8000/users/login", {
         method: "POST",
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: formData
+        body: formData,
       });
 
       const data = await res.json();
 
       if (res.ok) {
-
         localStorage.setItem("token", data.access_token);
 
         window.location.href = "/";
-
       } else {
-
         alert(data.detail || "Login failed");
-
       }
-
     } catch (error) {
-
       console.error(error);
 
       alert("Server error. Backend may not be running.");
-
     }
-
   };
 
   return (
-
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-
       <div className="bg-white p-8 rounded shadow w-80">
-
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          Login
-        </h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
 
         <input
           className="border p-2 w-full mb-3 rounded"
           placeholder="Email"
           value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
@@ -68,7 +52,7 @@ function Login() {
           className="border p-2 w-full mb-3 rounded"
           placeholder="Password"
           value={password}
-          onChange={(e)=>setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button
@@ -79,18 +63,17 @@ function Login() {
         </button>
 
         <p className="text-center mt-4">
-          Don't have an account? 
+          Don't have an account?
           <span
-          className="text-blue-600 cursor-pointer"
-          onClick={()=>window.location.href="/register"}>Register</span>
-          </p>
-
+            className="text-blue-600 cursor-pointer"
+            onClick={() => (window.location.href = "/register")}
+          >
+            Register
+          </span>
+        </p>
       </div>
-
     </div>
-
   );
-
 }
 
 export default Login;

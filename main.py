056cc42f56deaf4467159dee5dedc7b0e45c83cb
Ajay Kumar Reddy <reddyajay155@gmail.com>
2026-07-10 15:31:49 +0530

    fix CORS syntax

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=[
-    "http://localhost:3000",
-    "https://finsight-finance-analytics-dashboar.vercel.app",
-    "https://finsight-finance-analytics-dashboard-jrc4kwist-finsight3.vercel.app",
-]
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

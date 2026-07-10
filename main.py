ca96aa8e93be75e93fc396658aa0aad4ce576d7b
Ajay Kumar Reddy <reddyajay155@gmail.com>
2026-07-10 10:08:27 +0530

    prepare project for deployment

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 from fastapi import FastAPI
-from routes import users
-from routes import users, expenses
-from routes import analytics
 from fastapi.middleware.cors import CORSMiddleware
-from routes import budget
+
 from database import engine
 from models import Base
-from routes import insights
+from routes import users, expenses, analytics, budget, insights
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all frontend origins
+    allow_origins=[
+        "http://localhost:3000",  # Local React app
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(users.router)
-
-@app.get("/")
-def home():
-    return {"message": "Backend is working!"}
+Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router)
 app.include_router(expenses.router)
@@ -30,13 +25,6 @@ app.include_router(analytics.router)
 app.include_router(budget.router)
 app.include_router(insights.router)
 
-Base.metadata.create_all(bind=engine)
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")
+def home():
+    return {"message": "Backend is working!"}
